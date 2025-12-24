a9701802b0ca6bed405b0152256ad1e506066f0e
Felipe Santos de Amorim <255582023@eniac.edu.br>
2025-12-23 21:50:17 -0300

    arrumando link do sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,12 +1,13 @@
 import type { SideBarType } from '../types/sidebarTypes'
 import { HomeIcon, GlobeAltIcon, CalendarIcon, ClipboardIcon } from '@heroicons/react/24/outline'
+import { Link } from 'react-router-dom'
 
 function SideBar({className}: SideBarType){
     const links = [
         { name: "Estado", href: "/", icon: HomeIcon },
         { name: "Países", href: "/paises", icon: GlobeAltIcon },
-        { name: "Datas", href: "/datas", icon: CalendarIcon },
-        { name: "Formulário", href: "/formulario", icon: ClipboardIcon }
+        { name: "Datas", href: "/date", icon: CalendarIcon },
+        { name: "Formulário", href: "/form", icon: ClipboardIcon }
     ]
     return(
     <aside className={`fixed top-0 left-0 z-40 w-64 h-screen bg-neutral-primary-soft border-r border-default overflow-y-auto transition-transform ${className}`}>
@@ -16,10 +17,10 @@ function SideBar({className}: SideBarType){
             const Icon = link.icon
             return (
               <li key={link.name}>
-                <a href={link.href} className="flex items-center px-3 py-2 text-body rounded-md hover:bg-neutral-tertiary hover:text-fg-brand transition-colors">
+                <Link to={link.href} className="flex items-center px-3 py-2 text-body rounded-md hover:bg-neutral-tertiary hover:text-fg-brand transition-colors">
                   <Icon className="w-5 h-5 text-gray-400 group-hover:text-fg-brand" />
                   <span className="ml-3">{link.name}</span>
-                </a>
+                </Link>
               </li>
             )
           })}
